cafb4ea96c68ebc0d61fb753e455c82dace68d15
yash shah <68466768+yashshah2002@users.noreply.github.com>
2021-06-13 10:42:50 +0530

    ceorien.js

---
+++ 01 advance/coercion.js
@@ -0,0 +1,28 @@
+console.log('5' + 5);
+// const giveType = typeof {'5'}
+
+// console.log(giveType);
+
+const adder =  5 + false 
+
+// console.log(adder);
+
+
+const loginDetials = ''
+//login for getting details from DB
+
+const loginID = loginDetials[0]
+
+if (loginID !== undefined) {
+    console.log('Allow user to login');
+
+} else {
+    console.log('Auth failed');
+
+}
+
+// Values that interpret as false:
+// false
+// 0 
+// null 
+// undefined
